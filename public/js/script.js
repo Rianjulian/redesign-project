0dcd09f198c0307784e3f0c10e5e8293bba16a83
Rianjulian <harakiri048@gmail.com>
2022-04-26 16:36:34 +0700

    responsive

---
+++ public/js/script.js
@@ -0,0 +1,74 @@
+const hamburger = document.getElementById("hamburgerButton");
+const menu = document.getElementById("menu");
+const closeButton =  document.getElementById("closeButton");
+const nav = document.querySelector("nav");
+
+hamburger.addEventListener('click', function() {
+    menu.classList.remove('hidden');
+    // if (menu.classList === "hidden") {
+    //     menu.classList.remove("grid");
+    //   } else {
+    //     menu.classList.add('hidden');
+    //   }
+    });
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        navText: ["<span class='nav-btn prev-slide'><</span>", "<span class='nav-btn next-slide'>></span>"],
+        // margin: 10,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true,
+                autoplay: true,
+                smartSpeed: 800,
+                animateIn: 'linear',
+                animateOut: 'linear',
+                stagePadding: 60,
+                margin: 100,
+                nav: false,
+                dots: false
+            },
+            400: {
+                items: 1,
+                stagePadding: 80,
+                margin: 100,
+                nav: false,
+                dots: false,
+                autoplay: true,
+                smartSpeed: 800,
+                animateIn: 'fadeIn',
+                animateOut: 'fadeOut',
+            },
+            578: {
+                items: 2,
+                nav: false,
+                margin: 100,
+                stagePadding: 100,
+            },
+            700: {
+                items: 2,
+                nav: false,
+                dots: false,
+                loop: true,
+                stagePadding: 100,
+                margin: 100,
+                autoplay: true,
+                smartSpeed: 500,
+                animateIn: 'fadeIn',
+                animateOut: 'fadeOut',
+            },
+            1200: {
+                items: 2,
+                nav: false,
+                dots: false,
+                loop: true,
+                autoplay: true,
+                smartSpeed: 500,
+                animateIn: 'fadeIn',
+                animateOut: 'fadeOut',
+                stagePadding: 220,
+                margin: 220
+            }
+        }
+    })
